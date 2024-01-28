67e81d7724d325699fe68e9e66078199ad4b5012
gabrielyvg <gabrielyvg@outlook.com>
2024-01-28 18:12:24 -0300

    feat: estilizando o cadastrar cliente para deixar padrão com o pedidos

--- front/src/pages/clientes/cadastrar-cliente.js
+++ front/src/pages/clientes/cadastrar-cliente.js
@@ -49,64 +49,72 @@ export default function CadastrarCliente() {
     }
 
     return (
-        <>
-            <div className='grid grid-cols-12'>
-                <div className='grid-cols-2'>
-                    <button className='btn-back' onClick={voltar}>back
-                        {/* <FontAwesomeIcon icon="fa-solid fa-arrow-left" /> */}
-                    </button>
-                    <h1>Cadastrar Cliente</h1>
-                </div>
-                <div className='containerElements form'>
-                    <form onSubmit={onSubmit} className='items-center'>
-                        <div>
+        <section className="bg-neutral-100">
+            <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+                <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900">Cadastrar cliente</h2>
+                <form onSubmit={onSubmit} className='items-center'>
+                    <div className='grid grid-cols-2'>
+                        <div className='mr-4'>
+                            <label htmlFor="nome" className="block mb-2 text-sm font-medium text-gray-900">Nome cliente</label>
                             <input type="text"
                                 name="nome"
                                 placeholder="Nome"
                                 value={dadosFormulario.nome}
-                                onChange={handleInput} />
+                                onChange={handleInput}
+                                className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5' />
                         </div>
-                        <div>
+                        <div className='ml-4'>
+                            <label htmlFor="nome" className="block mb-2 text-sm font-medium text-gray-900">Telefone</label>
                             <input type="phone"
                                 name="telefone"
                                 placeholder="Telefone"
                                 value={dadosFormulario.telefone}
-                                onChange={handleInput} />
+                                onChange={handleInput}
+                                className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5' />
                         </div>
-                        <div>
+                        <div className='mr-4'>
+                            <label htmlFor="nome" className="block mb-2 text-sm font-medium text-gray-900">Endereço</label>
                             <input type="text"
                                 name="endereco"
                                 placeholder="Endereço"
                                 value={dadosFormulario.endereco}
-                                onChange={handleInput} />
+                                onChange={handleInput}
+                                className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5' />
                         </div>
-                        <div>
+                        <div className='ml-4'>
+                            <label htmlFor="nome" className="block mb-2 text-sm font-medium text-gray-900">Número</label>
                             <input type="text"
                                 name="numero"
                                 placeholder="Número"
                                 value={dadosFormulario.numero}
-                                onChange={handleInput} />
+                                onChange={handleInput}
+                                className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5' />
                         </div>
-                        <div>
+                        <div className='mr-4'>
+                            <label htmlFor="nome" className="block mb-2 text-sm font-medium text-gray-900">Bairro</label>
                             <input type="text"
                                 name="bairro"
                                 placeholder="Bairro"
                                 value={dadosFormulario.bairro}
-                                onChange={handleInput} />
+                                onChange={handleInput}
+                                className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5' />
                         </div>
-                        <div>
+                        <div className='ml-4'>
+                            <label htmlFor="nome" className="block mb-2 text-sm font-medium text-gray-900">Cidade</label>
                             <input type="text"
                                 name="cidade"
                                 placeholder="Cidade"
                                 value={dadosFormulario.cidade}
-                                onChange={handleInput} />
+                                onChange={handleInput}
+                                className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5' />
                         </div>
-                        <div>
-                            <button type="submit">Cadastrar</button>
-                        </div>
-                    </form>
-                </div>
+                    </div>
+                    <div className='flex justify-center mt-5'>
+                        <button className='border-solid border-2 text-white rounded-md px-5 py-1 mr-3 bg-gray-400 hover:bg-gray-500' onClick={voltar}>Voltar</button>
+                        <button className='border-solid border-2 text-white rounded-md px-5 py-1 bg-emerald-500 hover:bg-emerald-600' type="submit">Salvar</button>
+                    </div>
+                </form>
             </div>
-        </>
+        </section>
     );
 }
