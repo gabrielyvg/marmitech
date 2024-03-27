934a4ccedac429c8ec980ef049fca7eb412523d1
gabrielyvg <gabrielyvg@outlook.com>
2024-03-27 18:29:47 -0300

    refactor: adding a try catch to the save request and waiting for the response

--- front/src/services/produtoService.js
+++ front/src/services/produtoService.js
@@ -13,17 +13,25 @@ export const produtoService = {
     },
 
     async salvar(dados) {
-        const response = await fetch('http://localhost:3001/produtos/salvar', {
-            method: 'POST',
-            body: JSON.stringify(dados),
-            headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*'
-            },
-        }).then(async (respostaDoServidor) => {
-            if (!respostaDoServidor.ok) throw new Error('Erro ao salvar produto!')
-            const body = respostaDoServidor.body;
-            return body;
-        })
+        try {
+            const response = await fetch('http://localhost:3001/produtos/salvar', {
+                method: 'POST',
+                body: JSON.stringify(dados),
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*'
+                },
+            });
+    
+            if (!response.ok) {
+                throw new Error('Erro ao salvar produto!');
+            }
+            
+            const responseBody = await response.json();
+            return responseBody;
+        } catch (error) {
+            console.error('Erro ao salvar produto:', error);
+            throw error;
+        }
     }
 }
