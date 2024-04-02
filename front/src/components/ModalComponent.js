558a10c4f450a13778af6ac1c7b8c12a1fb5afe8
gabrielyvg <gabrielyvg@outlook.com>
2024-04-02 15:51:07 -0300

    fix: adjusting the style of the modal

--- front/src/components/ModalComponent.js
+++ front/src/components/ModalComponent.js
@@ -15,27 +15,34 @@ export default function ModalComponent({ nome, modalIsOpen, positiveAnswer, clos
       onAfterOpen={afterOpenModal}
       onRequestClose={closeModal}
       ariaHideApp={false}
-      className='w-96 h-40 bg-white m-auto justify-center items-center border-solid border-2 border-black rounded-xl'
+      className='fixed top-0 left-0 w-full h-full flex justify-center items-center z-50'
     >
-      <div className='flex justify-between bg-red-200 p-2 font-semibold text-red-700'>
-        <h1>Atenção</h1>
-        <button onClick={closeModal}>X</button>
-      </div>
-      <div className='p-2'>
-        <span className='flex '>
-          Tem certeza que deseja remover {nome}?
-        </span>
-        <div className='flex justify-end mt-10'>
-          <button
-            className='rounded-md px-4 py-1 bg-slate-300 mr-2'
-            onClick={closeModal}
-          >Não</button>
-          <button
-            className='rounded-md px-4 py-1 bg-slate-300'
-            onClick={handleConfirmation}
-          >Sim</button>
+      <div className='w-96 h-40 bg-white justify-center items-center shadow-sm shadow-black  rounded-xl'>
+        <div className='flex justify-between bg-red-200 p-2 font-semibold text-red-700 rounded-t-lg'>
+          <h1>Atenção</h1>
+          <button onClick={closeModal}>X</button>
+        </div>
+        <div className='p-2'>
+          <span className='flex'>
+            Tem certeza que deseja remover {nome}?
+          </span>
+          <div className='flex justify-end mt-10'>
+            <button
+              className='rounded-md px-4 py-1 bg-slate-300 mr-2'
+              onClick={closeModal}
+            >
+              Não
+            </button>
+            <button
+              className='rounded-md px-4 py-1 bg-slate-300'
+              onClick={handleConfirmation}
+            >
+              Sim
+            </button>
+          </div>
         </div>
       </div>
     </Modal>
+
   );
 }
