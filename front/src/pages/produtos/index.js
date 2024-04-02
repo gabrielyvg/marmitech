b7fe893ff60f541841dd4601c74c924ddabbcb4b
gabrielyvg <gabrielyvg@outlook.com>
2024-04-02 15:34:48 -0300

    feat:adding confirmation modal to remove the product

--- front/src/pages/produtos/index.js
+++ front/src/pages/produtos/index.js
@@ -4,10 +4,15 @@ import { produtoService } from '../../services/produtoService';
 import mock from '@/utils/mock';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { toast, ToastContainer } from 'react-nextjs-toast';
+import ModalComponent from '../../components/ModalComponent';
 
 export default function Produtos() {
     const router = useRouter();
     const [produtos, setClientes] = useState([]);
+
+    const [modalIsOpen, setIsOpen] = useState(false);
+    const [nome, setNome] = useState();
+    const [id, setId] = useState();
 
     useEffect(() => {
         setTimeout(() => {
@@ -51,9 +56,30 @@ export default function Produtos() {
         }
     }
 
+    const positiveAnswer = () => {
+        removerProduto(id);
+        setIsOpen(false);
+    };
+
+    const closeModal = () => {
+        setIsOpen(false);
+    };
+
+    const openModal = (nome, id) => {
+        setNome(nome);
+        setId(id)
+        setIsOpen(true);
+    }
+
     return (
         <>
             <section>
+                <ModalComponent
+                    nome={nome}
+                    modalIsOpen={modalIsOpen}
+                    positiveAnswer={positiveAnswer}
+                    closeModal={closeModal}
+                />
                 <button className='mt-3 ml-3 border-solid border-2 text-white rounded-md px-4 py-1 mr-3 bg-emerald-500 hover:bg-emerald-600' onClick={addProduto}>Adicionar produto</button>
                 <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
                     <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900">Listagem de produtos</h2>
@@ -91,7 +117,7 @@ export default function Produtos() {
                                                 icon="trash-can"
                                                 className='cursor-pointer text-red-700'
                                                 title='Excluir'
-                                                onClick={() => removerProduto(row.id)}
+                                                onClick={() => openModal(row.nome, row.id)}
                                             />
                                         </td>
                                     </tr>
