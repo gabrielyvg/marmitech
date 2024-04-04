40b530e983456a8f1a563e44b1f0a1df1079e2e4
gabrielyvg <gabrielyvg@outlook.com>
2024-04-04 15:31:41 -0300

    feat: adding the list checkbox of products and quantity

--- front/src/pages/pedidos/cadastrar-pedidos.js
+++ front/src/pages/pedidos/cadastrar-pedidos.js
@@ -18,9 +18,11 @@ export default function CadastrarPedidos() {
         pago: false,
     });
     const [clientes, setClientes] = useState([]);
+    const [produtos, setProdutos] = useState([]);
 
     useEffect(() => {
         getCliente();
+        getProdutos();
     }, []);
 
     const handleInput = (e) => {
@@ -60,6 +62,16 @@ export default function CadastrarPedidos() {
             })
     }
 
+    const getProdutos = async () => {
+        await produtoService.listar()
+            .then((response) => {
+                setProdutos(response);
+            })
+            .catch((error) => {
+                console.error(error);
+            })
+    }
+
     const voltar = () => {
         router.push('/pedidos/');
     }
@@ -83,7 +95,7 @@ export default function CadastrarPedidos() {
                         <div className='ml-4'>
                             <label htmlFor="cliente" className="block mb-2 text-sm font-medium text-gray-900">Selecionar Cliente</label>
                             <select id='cliente' name='cliente'
-                               /*  value={dadosFormulario.nome} */
+                                /*  value={dadosFormulario.nome} */
                                 onChange={handleInput}
                                 className='shadow-sm bg-gray-50 border
                                     border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5'
@@ -94,23 +106,44 @@ export default function CadastrarPedidos() {
                             </select>
                         </div>
 
-                        <div className='ml-4'>
-                            <label htmlFor="tamanho" className="block mb-2 text-sm font-medium text-gray-900">Tamanho marmita</label>
-                            <select id='tamanho' name='tamanho'
-                                value={dadosFormulario.tamanho}
-                                onChange={handleInput}
-                                className='shadow-sm bg-gray-50 border
-                                    border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5'
-                            >
-                                {mockObj.marmitasTamanho.map((mtTamanho) => (
-                                    <option key={mtTamanho.id}>{mtTamanho.tamanho}</option>
-                                ))}
-                            </select>
+                        <div className='flex flex-col mt-2'>
+                            <span className="block text-sm font-medium text-gray-900">Produtos</span>
+                            {
+                                produtos.map((produto) => (
+                                    <div className='grid grid-cols-2 items-center mb-2'>
+                                        <label htmlFor="produto" className="text-sm font-medium text-gray-900 mr-4" >
+                                            <input
+                                                name='produto'
+                                                type='checkbox'
+                                                value={produto.nome}
+                                                id={produto.id}
+                                                className='mr-2'
+                                            />
+                                            {produto.nome}
+                                        </label>
+                                        <div className=''>
+                                            <label className='w-28'>
+                                                <input
+                                                    type='number'
+                                                    min={0}
+                                                    placeholder='Quantidade'
+                                                    className='shadow-sm bg-gray-50 border border-gray-300 
+                                                    text-gray-900 text-sm rounded-lg 
+                                                    focus:ring-primary-500 focus:border-primary-500 
+                                                    block  p-2.5'
+                                                />
+                                            </label>
+                                        </div>
+
+                                    </div>
+                                ))
+                            }
                         </div>
-                        <div className='mt-4'>
+
+                           <div className='ml-4 mt-4'>
                             <DatePicker />
                         </div>
-                        <div className='mt-4'>
+                        <div className='ml-4 mt-4'>
                             <input type="checkbox" id="pago" name="pago" className='mr-1' checked={dadosFormulario.pago} onChange={handleInput} />
                             <label htmlFor="pago" className="text-sm font-medium text-gray-900" >Pago</label>
                         </div>
