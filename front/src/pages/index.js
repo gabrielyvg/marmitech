3048ce4180d1205e693d17d6d8ce68d627a493ce
gabrielyvg <gabrielyvg@outlook.com>
2024-04-09 20:16:12 -0300

    refactor: refactor login layout

--- front/src/pages/index.js
+++ front/src/pages/index.js
@@ -53,27 +53,58 @@ export default function Login() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <main>
-        <div className={styles.container}>
-          <div className={styles.box}>
+        <div className='fixed inset-0 flex items-center justify-center border rounded'>
+          <div className='w-full max-w-md p-6 rounded-md'>
             <div>
               <h1>{titulo}</h1>
-              <span className={styles.subtitulo}>{subtitulo}</span>
+              <span className="text-2xl font-semibold"> Bem-vindo de volta!</span>
             </div>
-            <div className='containerElements'>
-              <form onSubmit={onSubmit}>
-                <input onChange={handleChange} type="text"
-                  name="username" values={values.username}
-                  placeholder="Usuário" />
-                <input onChange={handleChange} type="password"
-                  name="password" value={values.password}
-                  placeholder="Senha" />
-                <button type="submit">Entrar</button>
-              </form>
-            </div>
+            <small className="text-indigo-600">Use o formulário abaixo para acessar sua conta</small>
+            <form className="grid mt-2" onSubmit={onSubmit}>
+              <input
+                onChange={handleChange}
+                type="text"
+                name="username"
+                values={values.username}
+                placeholder="Usuário"
+                className="p-4 mb-2 rounded-md focus:outline-indigo-600 shadow-md shadow-indigo-500/40"
+              />
+              <input
+                onChange={handleChange}
+                type="password"
+                name="password"
+                value={values.password}
+                placeholder="Senha"
+                className="p-4 mb-2 rounded-md focus:outline-indigo-600 shadow-md shadow-indigo-500/40"
+              />
+              <div className="flex justify-between mt-2">
+                <button
+                  type="button"
+                  className="text-indigo-600"
+                >
+                  Esqueceu a senha?
+                </button>
+                <button
+                  type="submit"
+                  className="bg-indigo-400 
+                    text-white p-2 w-32 
+                    rounded-md shadow-sm 
+                    shadow-indigo-600
+                    font-semibold
+                    hover:bg-slate-100 hover:text-black hover:border-solid hover:border-1 hover:border-indigo-400 hover:ease-in hover:duration-300"
+                >
+                  Entrar
+                </button>
+              </div>
+            </form>
+            <span className="flex place-content-center mt-6 text-indigo-600">Não possui conta?
+              <button className="font-semibold text-indigo-800">
+                Criar conta
+              </button>
+            </span>
           </div>
         </div>
       </main>
-
     </>
   );
 }
