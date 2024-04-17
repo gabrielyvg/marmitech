64efec24ab2a12308e4c1876bf8e41e351041c4c
gabrielyvg <gabrielyvg@outlook.com>
2024-04-17 20:09:17 -0300

    refactor: removing the unnecessary buttons

--- front/src/pages/clientes/index.js
+++ front/src/pages/clientes/index.js
@@ -81,7 +81,6 @@ export default function Clientes() {
                     positiveAnswer={positiveAnswer}
                     closeModal={closeModal}
                 />
-                <button className='mt-3 ml-3 border-solid border-2 text-white rounded-md px-4 py-1 mr-3 bg-emerald-500 hover:bg-emerald-600' onClick={addCliente}>Adicionar cliente</button>
                 <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
                     <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900">Listagem de clientes</h2>
                     <table className='table-auto border-separate sm:border-spacing-1 md:border-spacing-4 lg:border-spacing-6 border border-slate-300 rounded-lg'>
--- front/src/pages/produtos/index.js
+++ front/src/pages/produtos/index.js
@@ -26,10 +26,6 @@ export default function Produtos() {
         })
     }, []);
 
-    const addProduto = () => {
-        router.push('/produtos/cadastrar-produtos');
-    }
-
     const editarProduto = (idProduto) => {
         router.push(`/produtos/cadastrar-produtos/${idProduto}`);
     }
@@ -80,7 +76,6 @@ export default function Produtos() {
                     positiveAnswer={positiveAnswer}
                     closeModal={closeModal}
                 />
-                <button className='mt-3 ml-3 border-solid border-2 text-white rounded-md px-4 py-1 mr-3 bg-emerald-500 hover:bg-emerald-600' onClick={addProduto}>Adicionar produto</button>
                 <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
                     <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900">Listagem de produtos</h2>
                     <table className='table-auto border-separate sm:border-spacing-1 md:border-spacing-4 lg:border-spacing-6 border border-slate-300 rounded-lg'>
