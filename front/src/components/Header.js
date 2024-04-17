1a93ab4d4b558a07eea8862f901a36cdd16b4bf9
gabrielyvg <gabrielyvg@outlook.com>
2024-04-17 20:08:26 -0300

    refactor: changing the header menu to a side menu

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -1,21 +1,41 @@
 import Link from 'next/link';
 import mockObject from '../utils/mock';
-import React from "react";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSortDown } from '@fortawesome/free-solid-svg-icons';
 
 export default function Header() {
+    const [expandedMenuId, setExpandedMenuId] = useState(null);
+
+    const handleTitleClick = (id) => {
+        setExpandedMenuId(id === expandedMenuId ? null : id);
+    };
+    
     return (
-        <header className='w-full'>
-            <div className="h-10 sticky top-0 bg-stone-950">
-                <div className="flex items-center h-full">
-                    <nav className="text-white">
-                        {mockObject.menuOptions.map((menu) => (
-                            <React.Fragment key={menu.id}>
-                                <Link className="p-4" href={menu.url} >{menu.title}</Link>
-                            </React.Fragment>
-                        ))}
-                    </nav>
-                </div>
+        <aside className='fixed top-0 left-0 w-64 h-full shadow-lg shadow-black' aria-label="Sidenav">
+            <div>
+                <ul>
+                    {mockObject.menuOptions.map((menu) => (
+                        <li key={menu.id}>
+                            <div className="flex items-center justify-around cursor-pointer" onClick={() => handleTitleClick(menu.id)}>
+                                <span className="p-4 mr-4" >
+                                    {menu.title}
+                                </span>
+                                <FontAwesomeIcon
+                                    icon={faSortDown}
+                                />
+                            </div>
+                            {expandedMenuId === menu.id && (
+                                <ul className='m-2'>
+                                    <li key={menu.subitem.id}>
+                                        <Link className="p-4 ml-10" href={menu.subitem.url}>{menu.subitem.title}</Link>
+                                    </li>
+                                </ul>
+                            )}
+                        </li>
+                    ))}
+                </ul>
             </div>
-        </header>
+        </aside>
     );
 }
--- front/src/pages/_app.js
+++ front/src/pages/_app.js
@@ -1,16 +1,18 @@
 import "@/styles/globals.css";
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas, faTrashCan, faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import { fas, faTrashCan, faPenToSquare, faSortDown } from '@fortawesome/free-solid-svg-icons';
 import Header from "../components/Header";
 
-library.add(fas, faTrashCan, faPenToSquare);
+library.add(fas, faTrashCan, faPenToSquare, faSortDown);
 
 
 export default function App({ Component, pageProps }) {
     return (
         <>
             <Header />
-            <Component {...pageProps} />
+            <div className='ml-64'>
+                <Component {...pageProps} />
+            </div>
         </>
     );
 }
