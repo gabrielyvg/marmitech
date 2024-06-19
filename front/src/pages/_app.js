d8f16af8486e68642f24623a3d0a8374a18fe974
gabrielyvg <gabrielyvg@outlook.com>
2024-06-19 15:35:16 -0300

    refactor: adding conditionals to the Menu

--- front/src/pages/_app.js
+++ front/src/pages/_app.js
@@ -1,16 +1,19 @@
 import "@/styles/globals.css";
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas, faTrashCan, faPenToSquare, faSortDown } from '@fortawesome/free-solid-svg-icons';
-import Header from "../components/Header";
+import { fas, faTrashCan, faPenToSquare, faSortDown, faKitchenSet } from '@fortawesome/free-solid-svg-icons';
+import Menu from "../components/Menu";
+import { useRouter } from 'next/router';
 
-library.add(fas, faTrashCan, faPenToSquare, faSortDown);
-
+library.add(fas, faTrashCan, faPenToSquare, faSortDown, faKitchenSet);
 
 export default function App({ Component, pageProps }) {
+    const router = useRouter();
+    const showMenu = router.pathname !== '/';
+
     return (
         <>
-            <Header />
-            <div className='ml-64'>
+            {showMenu && <Menu />}
+            <div className='ml-72'>
                 <Component {...pageProps} />
             </div>
         </>
