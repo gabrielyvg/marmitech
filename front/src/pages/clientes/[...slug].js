34040ebfdc4caff60da3be6ffda8200de87842ff
gabrielyvg <gabrielyvg@outlook.com>
2024-08-20 20:59:51 -0300

    refactor: mudando os campos de inputs e checkboxes

--- front/src/pages/clientes/[...slug].js
+++ front/src/pages/clientes/[...slug].js
@@ -1,13 +1,17 @@
 import { useRouter } from 'next/router';
 import { clienteService } from '../../services/clienteService';
 import Buttons from '../../components/Buttons';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { toast, ToastContainer } from 'react-nextjs-toast'
+import { Toast } from 'primereact/toast';
+import { FloatLabel } from 'primereact/floatlabel';
+import { InputText } from 'primereact/inputtext';
+import { Checkbox } from 'primereact/checkbox';
 
 export default function CadastrarCliente() {
     const router = useRouter();
     const idCliente = router.query.slug && router.query.slug[1] ? router.query.slug[1] : null;
-
+    const toast = useRef(null);
     const [isLoading, setIsLoading] = useState(false);
 
     const [dadosFormulario, setDadosFormulario] = useState({
@@ -16,7 +20,7 @@ export default function CadastrarCliente() {
         bairro: '',
         cidade: '',
         endereco: '',
-        numero: 0,
+        numero: '',
         paga_mensalmente: false,
         paga_semanalmente: false,
         nfe: false,
@@ -44,17 +48,9 @@ export default function CadastrarCliente() {
                 data: dadosFormulario,
             })
 
-            toast.notify(result.mensagem, {
-                title: 'Salvo!',
-                duration: 3,
-                type: "success"
-            })
+            toast.current.show({ severity: 'success', summary: 'Success', detail: 'Salvo', life: 3000 });
         } catch (error) {
-            toast.notify(error.message, {
-                title: 'Erro!',
-                duration: 3,
-                type: "error"
-            })
+            toast.current.show({ severity: 'error', summary: 'Error', detail: 'Erro!', life: 3000 });
         } finally {
             setIsLoading(false);
             voltar();
@@ -81,102 +77,96 @@ export default function CadastrarCliente() {
             paga_mensalmente: fetchedClienteData.paga_mensalmente === 1 ? true : false,
             paga_semanalmente: fetchedClienteData.paga_semanalmente === 1 ? true : false
         };
-    
+
         setDadosFormulario(updatedData);
     }
 
     return (
-        <section>
+        <section className="flex justify-center">
             <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
                 <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900">Cadastrar cliente</h2>
                 <form onSubmit={onSubmit} className='items-center'>
                     <div className='grid grid-cols-2'>
                         <div className='mr-4'>
-                            <label htmlFor="nome" className="block mb-2 text-sm font-medium text-gray-900">Nome cliente</label>
-                            <input type="text"
-                                name="nome"
-                                placeholder="Nome"
-                                value={dadosFormulario.nome}
-                                onChange={handleInput}
-                                className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5' />
+                            <FloatLabel>
+                                <InputText type="text" className="p-inputtext-md"
+                                    name="nome"
+                                    value={dadosFormulario.nome}
+                                    onChange={handleInput} />
+                                <label htmlFor={'nome'}>Nome</label>
+                            </FloatLabel>
                         </div>
                         <div className='ml-4'>
-                            <label htmlFor="telefone" className="block mb-2 text-sm font-medium text-gray-900">Telefone</label>
-                            <input type="phone"
-                                name="telefone"
-                                placeholder="Telefone"
-                                value={dadosFormulario.telefone}
-                                onChange={handleInput}
-                                className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5' />
+                            <FloatLabel>
+                                <InputText type="text" className="p-inputtext-md"
+                                    name="telefone"
+                                    value={dadosFormulario.telefone}
+                                    onChange={handleInput} />
+                                <label htmlFor={'telefone'}>Telefone</label>
+                            </FloatLabel>
                         </div>
-                        <div className='mr-4'>
-                            <label htmlFor="endereco" className="block mb-2 text-sm font-medium text-gray-900">Endereço</label>
-                            <input type="text"
-                                name="endereco"
-                                value={dadosFormulario.endereco}
-                                placeholder="Endereço"
-                                onChange={handleInput}
-                                className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5' />
-                        </div>
-                        <div className='ml-4'>
-                            <label htmlFor="numero" className="block mb-2 text-sm font-medium text-gray-900">Número</label>
-                            <input type="text"
-                                name="numero"
-                                value={dadosFormulario.numero}
-                                placeholder="Número"
-                                onChange={handleInput}
-                                className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5' />
-                        </div>
-                        <div className='mr-4'>
-                            <label htmlFor="bairro" className="block mb-2 text-sm font-medium text-gray-900">Bairro</label>
-                            <input type="text"
-                                name="bairro"
-                                value={dadosFormulario.bairro}
-                                placeholder="Bairro"
-                                onChange={handleInput}
-                                className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5' />
-                        </div>
-                        <div className='ml-4'>
-                            <label htmlFor="cidade" className="block mb-2 text-sm font-medium text-gray-900">Cidade</label>
-                            <input type="text"
-                                name="cidade"
-                                value={dadosFormulario.cidade}
-                                placeholder="Cidade"
-                                onChange={handleInput}
-                                className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5' />
+                        <div className='mr-4 mt-4'>
+                            <FloatLabel>
+                                <InputText type="text"
+                                    name="endereco"
+                                    value={dadosFormulario.endereco}
+                                    onChange={handleInput} />
+                                <label htmlFor={'endereco'}>Endereço</label>
+                            </FloatLabel>
                         </div>
                         <div className='ml-4 mt-4'>
-                            <input type="checkbox"
-                                id="paga_mensalmente"
+                            <FloatLabel>
+                                <InputText type="text"
+                                    name="numero"
+                                    value={dadosFormulario.numero}
+                                    onChange={handleInput} />
+                                <label htmlFor={'numero'}>Número</label>
+                            </FloatLabel>
+                        </div>
+                        <div className='mr-4 mt-4'>
+                            <FloatLabel>
+                                <InputText type="text"
+                                    name="bairro"
+                                    value={dadosFormulario.bairro}
+                                    onChange={handleInput} />
+                                <label htmlFor={'bairro'}>Bairro</label>
+                            </FloatLabel>
+                        </div>
+                        <div className='ml-4 mt-4'>
+                            <FloatLabel>
+                                <InputText type="text"
+                                    name="cidade"
+                                    value={dadosFormulario.cidade}
+                                    onChange={handleInput} />
+                                <label htmlFor={'cidade'}>Cidade</label>
+                            </FloatLabel>
+                        </div>
+                        <div className="ml-4 mt-4 flex align-items-center">
+                            <Checkbox type="checkbox"
+                                inputId="paga_mensalmente"
                                 name="paga_mensalmente"
                                 value={dadosFormulario.paga_mensalmente}
                                 checked={dadosFormulario.paga_mensalmente}
-                                onChange={handleInput}
-                                className='mr-1'
-                            />
-                            <label htmlFor="paga_mensalmente" className="text-sm font-medium text-gray-900" >Paga mensalmente</label>
+                                onChange={handleInput} />
+                            <label htmlFor="paga_mensalmente" className="ml-2">Paga mensalmente</label>
                         </div>
-                        <div className='ml-4 mt-4'>
-                            <input type="checkbox"
-                                id="paga_semanalmente"
+                        <div className="ml-4 mt-4 flex align-items-center">
+                            <Checkbox type="checkbox"
+                                inputId="paga_semanalmente"
                                 name="paga_semanalmente"
                                 value={dadosFormulario.paga_semanalmente}
                                 checked={dadosFormulario.paga_semanalmente}
-                                onChange={handleInput}
-                                className='mr-1'
-                            />
-                            <label htmlFor="paga_semanalmente" className="text-sm font-medium text-gray-900" >Paga semanalmente</label>
+                                onChange={handleInput} />
+                            <label htmlFor="paga_semanalmente" className="ml-2">Paga semanalmente</label>
                         </div>
-                        <div className='ml-4 mt-4'>
-                            <input type="checkbox"
-                                id="nfe"
-                                name="nfe"
-                                value={dadosFormulario.nfe}
-                                checked={dadosFormulario.nfe}
-                                onChange={handleInput}
-                                className='mr-1'
-                            />
-                            <label htmlFor="nfe" className="text-sm font-medium text-gray-900" >Necessário tirar nota?</label>
+                        <div className="ml-4 mt-4 flex align-items-center">
+                            <Checkbox type="checkbox"
+                                 inputId="nfe"
+                                 name="nfe"
+                                 value={dadosFormulario.nfe}
+                                 checked={dadosFormulario.nfe}
+                                 onChange={handleInput} />
+                            <label htmlFor="nfe" className="ml-2">Necessário tirar nota?</label>
                         </div>
                     </div>
                     <Buttons
@@ -185,7 +175,7 @@ export default function CadastrarCliente() {
                     />
                 </form>
             </div>
-            <ToastContainer />
+            <Toast ref={toast} position="bottom-right" />
         </section>
     );
 }
