ac47cf70bb16f4b8138181ffc4a5b28ff0f78dbe
gabrielyvg <gabrielyvg@outlook.com>
2024-09-25 16:52:35 -0300

    feat: adding the remove and update functions to the instituicao, also adding the headerTable

--- front/src/pages/instituicao/index.js
+++ front/src/pages/instituicao/index.js
@@ -1,16 +1,25 @@
-import { Button } from "primereact/button";
 import { DataTable } from "primereact/datatable";
-import { IconField } from "primereact/iconfield";
-import { InputIcon } from "primereact/inputicon";
-import { InputText } from "primereact/inputtext";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Column } from "typeorm";
 import { instituicaoService } from "../../services/instituicaoService";
+import ActionButtonsTable from "../../components/ActionButtonsTable";
+import { FilterMatchMode, FilterOperator } from "primereact/api";
+import HeaderTable from "../../components/HeaderTable";
+import ModalComponent from "../../components/ModalComponent";
+import { useRouter } from "next/router";
+import { toast, ToastContainer } from 'react-nextjs-toast';
 
 export default function Instituicao() {
   const [instituicao, setInstituicao] = useState('');
-  const [filters, setFilters] = useState(null);
-  const [globalFilterValue, setGlobalFilterValue] = useState('');
+  const [filters, setFilters] = useState({
+    global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+    'nome': { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }] },
+  });
+
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [nome, setNome] = useState();
+  const [id, setId] = useState();
+  const router = useRouter();
 
   useEffect(() => {
     setTimeout(() => {
@@ -22,47 +31,79 @@ export default function Instituicao() {
         .catch((error) => {
           console.error(error);
         })
-      initFilters();
     })
   }, []);
 
-  const clearFilter = () => {
-    initFilters();
+  const positiveAnswer = () => {
+    removerCliente(id);
+    setIsOpen(false);
   };
 
-  const onGlobalFilterChange = (e) => {
-    const value = e.target.value;
-    let _filters = { ...filters };
-
-    _filters['global'].value = value;
-
-    setFilters(_filters);
-    setGlobalFilterValue(value);
+  const closeModal = () => {
+    setIsOpen(false);
   };
 
-  const initFilters = () => {
-    setFilters({
-      global: { value: null, matchMode: FilterMatchMode.CONTAINS },
-      'nome': { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }] },
-    });
-    setGlobalFilterValue('');
+  const openModal = (nome, id) => {
+    setNome(nome);
+    setId(id)
+    setIsOpen(true);
+  }
+
+  const editarInstituicao = (idInstituicao) => {
+    router.push(`/instituicao/cadastrar-instituicao/${idInstituicao}`);
+  }
+
+  const removerCliente = async (id) => {
+    try {
+      const result = await instituicaoService.remover({
+        id: id,
+      });
+
+      toast.notify(result.mensagem, {
+        title: 'Removido com sucesso!',
+        duration: 3,
+        type: "success"
+      })
+    } catch (error) {
+      toast.notify(error.message, {
+        title: 'Erro!',
+        duration: 3,
+        type: "error"
+      })
+    } finally {
+      router.reload();
+    }
+  }
+
+  const actionBodyTemplate = (item) => {
+    return (
+      <React.Fragment>
+        <ActionButtonsTable
+          nome={item.nome}
+          id={item.id}
+          editar={editarInstituicao}
+          openModal={openModal}
+        />
+      </React.Fragment>
+    );
   };
 
   const renderHeader = () => {
     return (
-      <div className="flex justify-content-between">
-        <Button type="button" icon="pi pi-filter-slash" label="Limpar" outlined onClick={clearFilter} />
-        <IconField iconPosition="left">
-          <InputIcon className="pi pi-search" />
-          <InputText value={globalFilterValue} onChange={onGlobalFilterChange} placeholder="Pesquisa" />
-        </IconField>
-      </div>
+      <HeaderTable filtros={filters} onFilterChange={setFilters} />
     );
-  };
+  }
+
   const header = renderHeader();
   return (
     <>
       <section>
+        <ModalComponent
+          nome={nome}
+          modalIsOpen={modalIsOpen}
+          positiveAnswer={positiveAnswer}
+          closeModal={closeModal}
+        />
         <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
           <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900">Listagem de Instituições</h2>
           <div className='overflow-x-auto'>
