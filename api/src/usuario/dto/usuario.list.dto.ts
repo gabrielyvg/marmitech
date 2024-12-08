26f4e9c6aadf956831173f13fb9d01c9b1287e88
gabrielyvg <gabrielyvg@outlook.com>
2024-12-08 15:10:34 -0300

    feat: adding a list DTO to user

---
+++ api/src/usuario/dto/usuario.list.dto.ts
@@ -0,0 +1,10 @@
+
+export interface UsuarioListDTO {
+  id: number;
+  nome: string;
+  telefone: string;
+  tipoPessoa: number;
+  idInstituicao: number;
+  email: string;
+  tipoUsuario: string;
+}
